9786993a242f018d7812781d7b9228073181a202
matheusfrancisco <mfranciscobatistamachado@gmail.com>
2019-02-20 18:20:30 -0300

    Source code blog

---
+++ content/1 - Frist post blog arara makerspace.markdown
@@ -0,0 +1,26 @@
+Title: Welcome to Arara makerspace blog.
+image: "/images/notfound.png"
+Date: 2018-02-20 16:48:47 +0530
+Tags: frist post, blog,
+Category: intro
+Slug: my-frist-post
+Author: Mataheus Francisco
+
+
+
+# My frist post
+
+
+This is a araramaker blog here we will to build news and beutiful things for free and the best things
+is anybody can to contribuite with post.
+
+
+
+## How I contribuite ?
+
+
+
+
+
+#### Hug's araras!
+
---
+++ pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Araras makers'
+SITENAME = 'Arara Makerspace'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'pelican-themes/pelican-blue'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+
+SIDEBAR_DIGEST = 'Arara makerspace the best place in the world'
+
+FAVICON = 'url-to-favicon'
+
+DISPLAY_PAGES_ON_MENU = True
+
+TWITTER_USERNAME = 'twitter-user-name'
+
+MENUITEMS = (('Blog', SITEURL),('Home', SITEURL),('Help-us', SITEURL))
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
